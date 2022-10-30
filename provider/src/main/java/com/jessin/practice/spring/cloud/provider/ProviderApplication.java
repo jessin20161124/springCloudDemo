4e7fc7c8e8127ce8b0c0f620f8f46f1d1672970a
guozexin <2995272029@qq.com>
2022-10-30 18:47:38 +0800

    统一revision

--- provider/src/main/java/com/jessin/practice/spring/cloud/provider/ProviderApplication.java
+++ provider/src/main/java/com/jessin/practice/spring/cloud/provider/ProviderApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * spring cloud服务注册，使用spring boot actuator做健康检查...需要保证向consul注册的健康检查url是正确的，
  * actuator会使用servlet biz path前缀
  * https://blog.csdn.net/weixin_34315189/article/details/91882436
+ *
+ * maven父子模块统一版本：
+ * https://blog.csdn.net/ZYC88888/article/details/112842029
  */
 @SpringBootApplication
 public class ProviderApplication {
